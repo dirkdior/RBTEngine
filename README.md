eb939eba7084272b3b96fe3495d6589084127fa4
dirkdior <dirkdior@gmail.com>
2021-04-01 10:01:53 +0100

    Update README.md Make siteIds a List in CampaignCache.scala

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-## Real time bidding engine
+# Real time bidding engine
+
+## Paths
+To submit a bid, send a POST request to `http://127.0.0.1:8080/bid/create` once the has started running
+
+## How to validate the solution
+Include any of the siteIds from the list `val siteIds: List[String]` in ` trait CampaignCacheT`
+
+Include a device.geo or user.geo country
+
+Include at least one `Impression`
+
+Include a `w` or `h` in the `Impression`, or any max or min combination
+
+Check that the bidFloor in the `BidRequest` is lower than that of the `Campaign`
+
+## Samples
+Based on the Campaigns and SiteIds in `CampaignCacheT`
+
+Here is a sample valid `BidRequest` that would return a `BidResponse`
+```
+{
+  "id": "SGu1Jpq1IO",
+  "site": {
+	"id": "747efc871b601ff343e2b861505dab04",
+	"domain": "fake.tld"
+  },
+  "device": {
+	"id": "440579f4b408831516ebd02f6e1c31b4",
+	"geo": {
+  	    "country": "LT"
+	}
+  },
+  "imp": [
+	{
+        "id": "1",
+        "wmin": 50,
+        "wmax": 300,
+        "hmin": 100,
+        "hmax": 300,
+        "h": 21,
+        "w": 27,
+        "bidFloor": 3.12123
+	},
+    {
+        "id": "2",
+        "wmin": 50,
+        "wmax": 300,
+        "hmin": 100,
+        "hmax": 300,
+        "h": 21,
+        "w": 25,
+        "bidFloor": 3.12123
+    }
+  ],
+  "user": {
+	"geo": {
+  	    "country": "LT"
+	},
+	"id": "USARIO1"
+  }
+}
+```
+
+The `BidResponse`:
+```
+{
+    "adid": "101",
+    "banner": {
+        "height": 21,
+        "id": 3,
+        "src": "stuff3",
+        "width": 25
+    },
+    "bidRequestId": "SGu1Jpq1IO",
+    "id": "someRandomId",
+    "price": 5.0
+}
+```
--- src/main/scala/com/rtbengine/core/CampaignCache.scala
+++ src/main/scala/com/rtbengine/core/CampaignCache.scala
@@ -7,9 +7,10 @@ import BiddingHandler._
 
 private [core] trait CampaignCacheT {
 
-  val siteIdOne   = md5Hash(randomUUID.toString)
-  val siteIdTwo   = md5Hash(randomUUID.toString)
-  val siteIdThree = md5Hash(randomUUID.toString)
+  val siteIds: List[String] = List("41725c9f64d3bdb62ef2727559f45b04", "7662271e8466735500f91210c9a008a6", "d21b44cc9cd3ac64e61ad79024f9cd7d",
+                                    "299c09204d370604ee319d9e3e7cfca4", "856ec07f708c471860b9ff9013a1aba9", "559c8650881c36d5c7281817c98752b5",
+                                    "f09463c798f4543343d3a7ad36134a79", "a02ca6a4b621456ca6780203af95b888", "46f50cd7afc5b8adfa575b8db361819a",
+                                    "747efc871b601ff343e2b861505dab04", "e321787c9da42d241614583e4f2c616b", "7b352dd8ed8f75068dee5a6528fc081a")
 
   var siteIdMap: Map[siteId, List[Campaign]] = Map.empty[siteId, List[Campaign]]
   val campaignCache: Seq[Campaign]           = setCampaigns
@@ -24,8 +25,6 @@ private [core] trait CampaignCacheT {
         siteIdMap += (siteId -> List(campaign))
     }
 
-  siteIdMap.foreach(println)
-
   def md5Hash(value: String): String = MessageDigest.getInstance("MD5").digest(value.getBytes).map("%02x".format(_)).mkString
 
   def setCampaigns: Seq[Campaign] = List(
@@ -33,10 +32,7 @@ private [core] trait CampaignCacheT {
       id        = 101,
       country   = "LT",
       targeting = Targeting(
-        targetedSiteIds = List(
-          siteIdOne,
-          siteIdTwo
-        )
+        targetedSiteIds = siteIds.slice(0, 10)
       ),
       banners   = List(
         Banner(id = 1, src = "stuff1", width = 21, height = 21),
@@ -49,10 +45,7 @@ private [core] trait CampaignCacheT {
       id        = 201,
       country   = "NG",
       targeting = Targeting(
-        targetedSiteIds = List(
-          siteIdOne,
-          siteIdThree
-        )
+        targetedSiteIds = siteIds.slice(2, 12)
       ),
       banners   = List(
         Banner(id = 1, src = "stuff1", width = 21, height = 21),
